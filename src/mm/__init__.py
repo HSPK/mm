56ca0e6d12b34762b28cc6dc3bd3ae67955ffd29
HSPK <whxway@whu.edu.cn>
2026-03-17 02:33:32 +0800

    use git vcs

--- src/mm/__init__.py
+++ src/mm/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("mm")
+except PackageNotFoundError:
+    try:
+        from mm._version import __version__  # type: ignore[no-redef]
+    except ImportError:
+        __version__ = "0.0.0-dev"
