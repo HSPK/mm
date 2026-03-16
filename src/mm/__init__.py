f2637719accf36ec7c80f5183e8b6a23e9952db1
HSPK <whxway@whu.edu.cn>
2026-03-17 02:35:35 +0800

    fix version

--- src/mm/__init__.py
+++ src/mm/__init__.py
@@ -1,9 +1,9 @@
-from importlib.metadata import PackageNotFoundError, version
-
 try:
-    __version__ = version("mm")
-except PackageNotFoundError:
+    from mm._version import __version__
+except ImportError:
+    from importlib.metadata import PackageNotFoundError, version
+
     try:
-        from mm._version import __version__  # type: ignore[no-redef]
-    except ImportError:
+        __version__ = version("mm")
+    except PackageNotFoundError:
         __version__ = "0.0.0-dev"
